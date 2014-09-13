38e9a1fad8af8c81dbb2d083b79402c7ed18d9b2
evilBird <travishenspeter@Traviss-MacBook-Pro-2.local>
2014-09-12 19:44:54 -0500

    intial docs

--- HBVLinkedTextView.h
+++ HBVLinkedTextView.h
@@ -8,9 +8,24 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ *  Block to be executed when a tap gesture is detected in a linked string
+ *
+ *  @param linkedString the string in which a tap gesture occurred
+ */
+
 typedef void (^LinkedStringTapHandler)(NSString *linkedString);
 
 @interface HBVLinkedTextView : UITextView
+
+/**
+ *  Detect a tap gesture on a specified string and perform a block
+ *
+ *  @param string                any substring of the textview's text property
+ *  @param defaultAttributes     an NSDictionary representing attributes to be applied to a linked string
+ *  @param highlightedAttributes an NSDictionary representing attributes to be applied to a linked string when it is tapped
+ *  @param tapHandler            a block of type ^LinkedStringTapHandler to be executed when the specified string is tapped.
+ */
 
 - (void)linkString:(NSString *)string defaultAttributes:(NSDictionary *)defaultAttributes highlightedAttributes:(NSDictionary *)highlightedAttributes tapHandler:(LinkedStringTapHandler)tapHandler;
 
